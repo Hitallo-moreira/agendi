69dbe0514e77434ab2dd1a8ad16dbaea7b1c22be
HitalloFeitosa <hitallo.feitosa@oficialfarma.com.br>
2024-03-16 09:44:46 -0300

    feat: add footer component and update package.json

---
+++ agendi/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const FooterElement = styled.footer`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    height: 60px;
+    width: 100%;
+    background-color: #05B387;
+    margin-top: 2rem;
+`;
+
+function Footer() {
+    return (
+        <FooterElement>
+            <p>Coded by Hitallo</p>
+        </FooterElement>
+    );
+}
+
+export default Footer;
